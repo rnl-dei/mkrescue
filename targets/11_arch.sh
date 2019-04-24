87aba4268b9e46518d081d4c39216e284cf5a306
André Breda <andre.breda@rnl.tecnico.ulisboa.pt>
2019-04-24 13:10:00 +0100

    targets/arch: Fix partition label in bootloader configuration
    Should we use UEFI by any reason, it will still boot. All configuration
    files are updated

--- targets/11_arch.sh
+++ targets/11_arch.sh
@@ -1,6 +1,6 @@
 NAME="Arch Live CD"
 
-PART_NAME="arch"
+PART_NAME="ARCH"
 
 DOWNLOAD_URL="http://ftp.rnl.tecnico.ulisboa.pt/pub/archlinux/iso/latest/"
 
@@ -9,4 +9,11 @@ function update_iso() {
 	local iso_path=$(curl -s "${DOWNLOAD_URL}/md5sums.txt" | awk '/.iso$/{print $2}')
 	ISO_FILE="${DATA_DIR}/$(basename $iso_path)"
 	download_file "${DOWNLOAD_URL}/$iso_path" "$ISO_FILE"
+}
+
+function customize() {
+	msg "Fixing archisolabel"
+	for f in ${PART_MOUNT_DIR}/**/boot/**/*.cfg ${PART_MOUNT_DIR}/loader/entries/*.conf; do
+		sed -i -E "s/archisolabel=ARCH_[0-9]+/archisolabel=$PART_NAME/g" $f
+	done
 }
