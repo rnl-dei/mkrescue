d64dd5d4abcd3316d3531304f4e0f451743955ee
André Breda <andre.breda@rnl.tecnico.ulisboa.pt>
2019-04-24 13:11:41 +0100

    targets/sysrescd: Update to new arch-based version
    Ports over the archisolabel fix from arch.

--- targets/05_sysrescd.sh
+++ targets/05_sysrescd.sh
@@ -1,12 +1,13 @@
 NAME="System Rescue CD"
 
-PART_NAME="sysrescd"
+PART_NAME="SYSRESCD"
 
 DOWNLOAD_URL="https://sourceforge.net/projects/systemrescuecd/files/latest/download?source=files"
 
-SYSLINUX_BIN=${ISO_MOUNT_DIR}/usb_inst/syslinux
-
 function customize() {
 	msg "Setting default keymap to PT"
-	sed -i "s/scandelay/setkmap=pt scandelay/g" ${PART_MOUNT_DIR}/syslinux/syslinux.cfg
+	msg "Fixing archisolabel"
+	for f in ${PART_MOUNT_DIR}/**/boot/**/*.cfg; do
+		sed -i -E "s/archisolabel=SYSRCD[0-9]+/archisolabel=$PART_NAME setkmap=pt-latin1/g" $f
+	done
 }
